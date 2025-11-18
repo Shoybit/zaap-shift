abb847dadd8af127f8615594643c4b741eeb84e6
Shoybit <shoyaib105@gmail.com>
2025-11-18 15:18:09 +0600

    password validation

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import RootLayout from "../Layouts/RootLayout";
 import Home from "../Pages/Home/Home";
 import Cover from "../Pages/Covargespages/Cover";
+import AuthLayout from "../Layouts/AuthLayout";
+import Login from "../Pages/Auth/Login";
+import Register from "../Pages/Auth/Register";
 
 export const router = createBrowserRouter([
   {
@@ -20,4 +23,19 @@ export const router = createBrowserRouter([
         
     ]
 },
+
+{
+    path: '/',
+    Component: AuthLayout,
+    children: [
+        {
+            path: 'login',
+            Component: Login
+        },
+        {
+            path: 'register',
+            Component: Register
+        }
+    ]
+}
 ]);
