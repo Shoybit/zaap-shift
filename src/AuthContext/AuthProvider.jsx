8dc1f95da804c4e05606cc4ee288915c038c883d
shoybit <shoyaib105@gmail.com>
2025-11-21 12:33:40 +0600

    logout

--- src/AuthContext/AuthProvider.jsx
+++ src/AuthContext/AuthProvider.jsx
@@ -1,35 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { 
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     GoogleAuthProvider,
-    signInWithPopup
+    signInWithPopup,
+    onAuthStateChanged,
+    signOut
 } from 'firebase/auth';
 import { auth } from '../Firebase/Firebase.int';
 
 const AuthProvider = ({ children }) => {
 
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
     // Register user with email/password
     const registerUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     };
 
     // Login user with email/password
     const signInUser = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     };
 
     // Login with Google
     const signInWithGoogle = () => {
+        setLoading(true);
         const provider = new GoogleAuthProvider();
         return signInWithPopup(auth, provider);
     };
 
+
+    // log out
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    // Track currently logged-in user
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
     const authInfo = {
+        user,
+        loading,
         registerUser,
         signInUser,
-        signInWithGoogle, // ✅ added Google login
+        signInWithGoogle,
+        logOut,
     };
 
     return (
