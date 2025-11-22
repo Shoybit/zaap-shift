bde7080fe720aa1a369e2cc1e619a116e8300627
shoybit <shoyaib105@gmail.com>
2025-11-22 10:19:20 +0600

    about Tabs  added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -15,7 +15,21 @@ const About = () => {
         </p>
       </div>
 
-
+      {/* Tabs Section */}
+      <div className="max-w-4xl mx-auto flex flex-wrap  gap-4 mb-12">
+        <button className="px-6 py-2  text-[#647925] font-bold text-2xl ">
+          Story
+        </button>
+        <button className="px-6 py-2 rounded-full  text-[#00000080] font-semibold text-2xl ">
+          Mission
+        </button>
+        <button className="px-6 py-2 rounded-full  text-[#00000080]  font-semibold text-2xl">
+          Success
+        </button>
+        <button className="px-6 py-2 rounded-full text-[#00000080] font-semibold text-2xl ">
+          Team & Others
+        </button>
+      </div>
 
 
       </div>
