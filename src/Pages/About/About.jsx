f762bc4e1a70ef271a98b36e36aeeef85920407e
shoybit <shoyaib105@gmail.com>
2025-11-22 10:19:02 +0600

    about header added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -15,27 +15,9 @@ const About = () => {
         </p>
       </div>
 
-      {/* Tabs Section */}
-      <div className="max-w-4xl mx-auto flex flex-wrap  gap-4 mb-12">
-        <button className="px-6 py-2  text-[#647925] font-bold text-2xl ">
-          Story
-        </button>
-        <button className="px-6 py-2 rounded-full  text-[#00000080] font-semibold text-2xl ">
-          Mission
-        </button>
-        <button className="px-6 py-2 rounded-full  text-[#00000080]  font-semibold text-2xl">
-          Success
-        </button>
-        <button className="px-6 py-2 rounded-full text-[#00000080] font-semibold text-2xl ">
-          Team & Others
-        </button>
-      </div>
 
-    <div className="max-w-4xl mx-auto flex flex-col gap-5">
-        <p className="text-[18px]">We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it's a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.</p>
-        <p className="text-[18px]">We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it's a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.</p>
-        <p className="text-[18px]">We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it's a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.</p>
-    </div>
+
+
       </div>
 
     </div>
